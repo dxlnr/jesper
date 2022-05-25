3c1fb601a4b1ebe159264ac93964dbca735ceca3
frommwonderland <daniel.illner@outlook.de>
2022-05-26 01:16:23 +0200

    started project

---
+++ scripts/main.py
@@ -0,0 +1,8 @@
+from src.data.raw import load_train_data
+
+def main():
+    load_train_data('../data/317_v4_train.parquet')
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/data/raw.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def load_train_data(path: str = './data/317_v4_train.parquet'):
+    r"""."""
+    train_data = pd.read_parquet(path)
+    print(train_data)
